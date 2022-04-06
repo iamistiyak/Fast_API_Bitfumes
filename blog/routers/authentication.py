20b5ad706a4a68567254ecf7075abb65d4ada688
iamistiyak <istiyak98215@gmail.com>
2022-04-06 12:58:29 +0530

    Authorisation behind route

--- blog/routers/authentication.py
+++ blog/routers/authentication.py
@@ -3,11 +3,12 @@ from fastapi.security import OAuth2PasswordRequestForm
 from .. import  schemas, database, models, token
 from ..hashing import Hash
 from sqlalchemy.orm import Session
+
 router = APIRouter(tags=['Authentication'])
 
 @router.post('/login')
-def login(request:schemas.Login, db: Session = Depends(database.get_db)):
-    user = db.query(models.User).filter(models.User.email == request.useremail).first()
+def login(request:OAuth2PasswordRequestForm = Depends(), db: Session = Depends(database.get_db)):
+    user = db.query(models.User).filter(models.User.email == request.username).first()
     if not user:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"Invalid Credentials")
